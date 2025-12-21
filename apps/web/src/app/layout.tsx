c9ca6e5573b4985c743a22014150d066b1082472
Andrew Bierman <94939237+andrew-bierman@users.noreply.github.com>
2025-12-21 07:37:20 -0700

    🎨 Cleanup

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import { Providers } from 'web-app/components/providers/providers'
-import './globals.css'
+import '@workspace/ui/globals.css'
 
 const geistSans = Geist({
 	variable: '--font-geist-sans',
