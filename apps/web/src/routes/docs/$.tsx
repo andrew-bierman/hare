7c6ad376e60ec400add77d799ea94ad9ced059fa
Andrew Bierman <abbierman101@gmail.com>
2025-12-31 09:03:25 -0500

    ♻️ Fix checks

--- apps/web/src/routes/docs/$.tsx
+++ apps/web/src/routes/docs/$.tsx
@@ -4,9 +4,9 @@ import { DocsLayout } from 'fumadocs-ui/layouts/docs'
 import defaultMdxComponents from 'fumadocs-ui/mdx'
 import { DocsBody, DocsDescription, DocsPage, DocsTitle } from 'fumadocs-ui/page'
 import { RootProvider } from 'fumadocs-ui/provider/tanstack'
-import { source } from '../../lib/docs/source'
 import { getLayoutOptions } from '../../lib/docs/layout.shared'
 import { getMDXComponents } from '../../lib/docs/mdx-components'
+import { source } from '../../lib/docs/source'
 import '../../styles/docs.css'
 
 export const Route = createFileRoute('/docs/$')({
@@ -22,18 +22,25 @@ export const Route = createFileRoute('/docs/$')({
 			title: page.data.title,
 			description: page.data.description,
 			toc: page.data.toc,
-			pageTree: source.pageTree,
+			pageTree: await source.serializePageTree(source.pageTree),
 			MDX,
 		}
 	},
 })
 
 function Page() {
-	const { title, description, toc, pageTree, MDX } = Route.useLoaderData()
+	const loaderData = Route.useLoaderData()
+	const { title, description, toc, pageTree, MDX } = loaderData as {
+		title: string | undefined
+		description: string | undefined
+		toc: unknown
+		pageTree: Root
+		MDX: React.ComponentType
+	}
 
 	return (
 		<RootProvider>
-			<DocsLayout {...getLayoutOptions()} tree={pageTree as Root}>
+			<DocsLayout {...getLayoutOptions()} tree={pageTree}>
 				<DocsPage toc={toc}>
 					<DocsTitle>{title}</DocsTitle>
 					<DocsDescription>{description}</DocsDescription>
